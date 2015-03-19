ff935b5757946f74d6648f25d13d4dde2d56760f
u13073878 <u13073878@tuks.co.za>
2015-03-19 20:25:14 +0200

    An experimental, proposed schema for the Resources database

---
+++ Notes/ProposedSchema.js
@@ -0,0 +1,10 @@
+var resourceSchema = new Schema({
+
+	ResourceID: Number,	
+	UserID: String,
+	ResourceName: String,
+	MIMEType: String,
+	UploadDate: Date.now
+	ResourceURL: String,
+	filePath: String
+});
