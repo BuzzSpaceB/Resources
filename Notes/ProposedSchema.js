d67859ffdfad8c0dabbf01b57611c5fea2126cb6
GoodnessTuks <u13046412@tuks.co.za>
2015-03-21 13:58:54 +0200

    Added to proposed schema

--- Notes/ProposedSchema.js
+++ Notes/ProposedSchema.js
@@ -3,6 +3,7 @@ var resourceSchema = new Schema({
 	resourceID: Number,	
 	userID: String,
 	resourceName: String,
+	resourceDescription: String,
 	mimeType: String,
 	fileSize: Number,
 	uploadDate: Date.now
