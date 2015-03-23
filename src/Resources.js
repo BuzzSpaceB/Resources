ca664b24bb49d8405322537afe28607dd28fb447
u13073878 <u13073878@tuks.co.za>
2015-03-23 16:10:37 +0200

    Added more function stubs to Resources.js

--- src/Resources.js
+++ src/Resources.js
@@ -1,5 +1,41 @@
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/');
+
+var schema = new mongoose.Schema({
+	resourceID: Integer,
+	userID: String,
+	resourceName: String,
+	resourceDescription: String,
+	mimeType: String,
+	fileSize: Double,
+	uploadDate: {type: Date, default: Date.now},
+	lastViewed: Date
+	resourceURL: String,
+	//filePath: String
+});
+
+var ResourceTypeContraintsManager = {};
+
+manageResourceTypeContraints.addResourceType = function()
+{
+		
+}
+
+manageResourceTypeContraints.removeResourceType = function()
+{
+	
+}
+
+manageResourceTypeContraints.modifyResourceType = function()
+{
+	
+}
+
 var Resources = {};
+
+Resources.manageResourceTypeContraints = ResourceTypeContraintsManager;
 
 Resources.uploadResources = function(file)
 {
@@ -10,6 +46,5 @@ Resources.deleteResources = function()
 {
 	
 }
-
 
 module.exports = Resources;
